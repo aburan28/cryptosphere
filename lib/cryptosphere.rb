bd7c9fa98a82f7e21c5d805bed5e56c817df696e
Tony Arcieri <tony.arcieri@gmail.com>
2013-07-27 19:21:18 -0700

    Replace PktLine with PktLineReader
    PktLines provide a useful structuring mechanism for streaming I/O. Reimplement
    the PktLine class in terms of a reader that first reads length prefixes then
    reads the requested amount of data, up to PKT_LINE_MAX.

--- lib/cryptosphere.rb
+++ lib/cryptosphere.rb
@@ -2,7 +2,7 @@ require 'cryptosphere/version'
 
 require 'cryptosphere/block'
 require 'cryptosphere/encoding'
-require 'cryptosphere/pkt_line'
+require 'cryptosphere/pkt_line_reader'
 require 'cryptosphere/position'
 
 require 'cryptosphere/heads/read_head'
@@ -14,14 +14,11 @@ module Cryptosphere
   # Size of symmetric keys in bytes (32 bytes, 256-bits)
   SECRET_KEY_BYTES = Crypto::SecretBox::KEYBYTES
 
-  # Secure random data source
-  def self.random_bytes(size)
-    Crypto::Random.random_bytes(size)
-  end
+  # The length prefix specifies a data segment longer than PKT_LEN_MAX
+  class LengthError < ArgumentError; end
 
-  def self.logger
-    Celluloid.logger
-  end
+  # An unexpected message was encountered
+  class ProtocolError < ArgumentError; end
 
   # Request to do something we're incapable of
   class CapabilityError < StandardError; end
@@ -31,4 +28,13 @@ module Cryptosphere
 
   # Implausible timestamps (i.e. ones from the future)
   class InvalidTimestampError < StandardError; end
+
+  # Secure random data source
+  def self.random_bytes(size)
+    Crypto::Random.random_bytes(size)
+  end
+
+  def self.logger
+    Celluloid.logger
+  end
 end
